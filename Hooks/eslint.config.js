f99a19d1829105dbed9a9dc7f34d79a438672aa1
pravinpatil1047 <pravinpatil1047@gmail.com>
2025-08-16 14:11:02 +0530

    made changes  in esLint.config.js

--- Hooks/eslint.config.js
+++ Hooks/eslint.config.js
@@ -7,11 +7,11 @@ import { defineConfig, globalIgnores } from 'eslint/config'
 export default defineConfig([
   globalIgnores(['dist']),
   {
-    files: ['**/*.{js,jsx}'],
-    extends: [
-      js.configs.recommended,
-      reactHooks.configs['recommended-latest'],
-      reactRefresh.configs.vite,
+      files: ['**/*.{js,jsx}'],
+      extends: [
+        js.configs.recommended,
+        reactHooks.configs['recommended-latest'],
+        reactRefresh.configs.vite,
     ],
     languageOptions: {
       ecmaVersion: 2020,
